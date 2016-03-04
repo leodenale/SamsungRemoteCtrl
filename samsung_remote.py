a85134f6f2e1d0481d2e74ef1ad7b60ae63e294a
Pedro Scarapicchia Junior <pedrinho.rep51@gmail.com>
2016-03-04 00:06:31 -0300

    Argument parser added

--- samsung_remote.py
+++ samsung_remote.py
@@ -1,38 +1,45 @@
 import time
 import socket
 import base64
+import argparse
 
+encoding = 'utf-8'
 src      = b'10.0.1.3'          # ip of remote
 mac      = b'00-AB-11-11-11-11' # mac of remote
 remote   = b'python remote'     # remote name
-dst      = '10.0.1.13'          # ip of tv
+dst      = '10.0.1.13'           # ip of tv
 app      = 'python'             # iphone..iapp.samsung
 tv       = 'LE32C650'           # iphone.LE32C650.iapp.samsung
-encoding = 'utf-8'
 port     = 55000
+
+def scan_network():
+    print("Scanning network...")
 
 def push(key):
   try:
     new = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     new.connect((dst, port))
+
     byte_key = bytes(key, encoding)
 
-    msg =  chr(0x64) + chr(0x00) +\
-           chr(len(base64.b64encode(src).decode(encoding))) +\
-           chr(0x00) + base64.b64encode(src).decode(encoding) +\
-           chr(len(base64.b64encode(mac).decode(encoding))) +\
-           chr(0x00) + base64.b64encode(mac).decode(encoding) +\
-           chr(len(base64.b64encode(remote).decode(encoding))) +\
-           chr(0x00) + base64.b64encode(remote).decode(encoding)
+    encoded_key    = base64.b64encode(byte_key).decode(encoding)
+    encoded_src    = base64.b64encode(src).decode(encoding)
+    encoded_mac    = base64.b64encode(mac).decode(encoding)
+    encoded_remote = base64.b64encode(remote).decode(encoding)
+    
+    msg =  chr(0x64) + chr(0x00)    +\
+           chr(len(encoded_src))    + chr(0x00) + encoded_src +\
+           chr(len(encoded_mac))    + chr(0x00) + encoded_mac +\
+           chr(len(encoded_remote)) + chr(0x00) + encoded_remote 
 
-    pkt =  chr(0x00) + chr(len(app)) + chr(0x00) + app + chr(len(msg)) +\
-           chr(0x00) + msg
+    pkt =  chr(0x00) +\
+           chr(len(app)) + chr(0x00) + app +\
+           chr(len(msg)) + chr(0x00) + msg
 
     new.send(bytes(pkt, encoding))
 
     msg =  chr(0x00) + chr(0x00) + chr(0x00) +\
-           chr(len(base64.b64encode(byte_key).decode(encoding))) +\
-           chr(0x00) + base64.b64encode(byte_key).decode(encoding)
+           chr(len(encoded_key)) + chr(0x00) + encoded_key
 
     pkt =  chr(0x00) + chr(len(tv)) + chr(0x00) + tv + chr(len(msg)) +\
            chr(0x00) + msg
@@ -45,10 +52,14 @@ def push(key):
     return False
 
 def main():
-  if push('PING'):
-    print ('pingo que eh uma blza')
-    push('KEY_VOLDOWN')
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-s", "--scan", help="scans the TV on yhe network", action="store_true")
+  parser.add_argument("-k", "--key", help="the key to be sent to TV", default="KEY_VOLDOWN")
+  args = parser.parse_args()
+
+  if args.scan:
+     scan_network()
   else:
-    print ('ti fode trouxa')
+     push(args.key)
 
 if __name__ == "__main__": main()
