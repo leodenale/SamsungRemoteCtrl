2ce513fdd88704ee381f441e2f868f89fbab8538
Pedro Scarapicchia Junior <pedrinho.rep51@gmail.com>
2017-04-15 03:39:10 -0300

    Adjustments for Mac OS X

--- samsung_remote.py
+++ samsung_remote.py
@@ -17,11 +17,12 @@ tv       =  'LE32C650'          # iphone.LE32C650.iapp.samsung
 port     =  55000
 
 # got this code from https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib/28950776#28950776
+# just need to change port 0 to 1 for some reason to work on Mac OS X
 def get_my_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 0))
+        s.connect(('10.255.255.255', 1))
         ip = s.getsockname()[0]
     except:
         ip = '127.0.0.1'
