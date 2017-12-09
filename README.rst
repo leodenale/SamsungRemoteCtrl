9c3b03efea51060106aa8644a96ab29f23a091ee
Pedro Scarapicchia Junior <pedrinho.rep51@gmail.com>
2017-12-09 01:34:39 -0200

    Adjustments in README and arg_parse

--- README.rst
+++ README.rst
@@ -7,30 +7,29 @@ A Samsung TV Remote Control Python Script
 Description
 ===========
 
-samsung_remote is a script writen in Python that can remotely control your Samsung Smart TV thru wifi. It supports some nice features, such as:
+samsung_remote is a script writen in Python that can remotely control your Samsung Smart TV thru wifi. It uses the great `samsungctl <https://github.com/Ape/samsungctl>`_ project to send the commands to TV and supports some nice features, such as:
 
 - scan the network to find all available TV's;
-- turn off all the found TV's with one command;
-- specify which TV's to send commands;
+- turn off all the TV's with one command;
+- specify which TV to send commands;
 - save common routines in a macro file to be executed latter. (video of a macro being executed: https://youtu.be/UXxBB7BOMDM)
 
 Usage
 =====
 
-usage: samsung_remote.py [-h] [-s] [-k KEY] [-p] [-m MACRO] [-l] [-q]
-                         [-i IP | -a]
+usage: samsung_remote.py [-h] [-a | -i ip] [-k key] [-l] [-m <file>] [-p] [-q]
+                         [-s]
 
 optional arguments:
-  -h, --help            show this help message and exit
-  -s, --scan            scans the TV on the network
-  -k KEY, --key KEY     the key to be sent to TV
-  -p, --poweroff        search all TV's in the network and turn them off
-  -m MACRO, --macro MACRO
-                        the macro file with commands to be sent to TV
-  -l, --legacy          use legacy method instead of default mode (websocket)
-  -q, --quiet           do not print messages to console
-  -i IP, --ip IP        defines the ip of the TV that will receive the command
-  -a, --auto            sends the command to the first TV available
+  -h, --help  show this help message and exit
+  -a          sends the command to the first TV available
+  -i ip       defines the ip of the TV that will receive the command
+  -k key      the key to be sent to TV
+  -l          use legacy method instead of default mode (websocket)
+  -m <file>   the macro file with commands to be sent to TV
+  -p          search all TV's in the network and turn them off
+  -q          do not print messages to console
+  -s          scans the network and print all the TV's found
 
 Dependecies
 ===========
@@ -47,6 +46,5 @@ References
 ==========
 
 - SSDP discovery https://gist.github.com/dankrause/6000248 and http://forum.micasaverde.com/index.php?topic=7878.15
-- https://gist.github.com/danielfaust/998441
 - Regular expression to get the XML namespace https://stackoverflow.com/a/12946675/2383657 
 - Correctly parse XML with namespaces https://codereview.stackexchange.com/a/51132
--- samsung_remote.py
+++ samsung_remote.py
@@ -117,43 +117,46 @@ def loadLog(quiet):
 def main():
     parser = argparse.ArgumentParser(
         description='Controls your Samsumg SmartTV thru Wifi')
-    parser.add_argument("-s", "--scan", action="store_true",
-                        help="scans the TV on the network")
-    parser.add_argument("-k", "--key",
-                        help="the key to be sent to TV")
-    parser.add_argument(
-        "-p",
-        "--poweroff",
-        action="store_true",
-        help="search all TV's in the network and turn them off")
-    parser.add_argument("-m", "--macro",
-                        help="the macro file with commands to be sent to TV")
-    parser.add_argument(
-        "-l",
-        "--legacy",
-        action="store_true",
-        help="use legacy method instead of default mode (websocket)")
-    parser.add_argument("-q", "--quiet", action="store_true",
-                        help="do not print messages to console")
     group = parser.add_mutually_exclusive_group()
+    group.add_argument('-a', action='store_true',
+                       help='sends the command to the first TV available')
     group.add_argument(
-        "-i",
-        "--ip",
-        help="defines the ip of the TV that will receive the command")
-    group.add_argument("-a", "--auto", action="store_true",
-                       help="sends the command to the first TV available")
+        '-i',
+        metavar='ip',
+        help='defines the ip of the TV that will receive the command')
 
+    parser.add_argument('-k', metavar='key', help='the key to be sent to TV')
+    parser.add_argument(
+        '-l',
+        action='store_true',
+        help='use legacy method instead of default mode (websocket)')
+    parser.add_argument(
+        '-m',
+        metavar='<file>',
+        help='the macro file with commands to be sent to TV')
+    parser.add_argument(
+        '-p',
+        action='store_true',
+        help="search all TV's in the network and turn them off")
+    parser.add_argument(
+        '-q',
+        action='store_true',
+        help='do not print messages to console')
+    parser.add_argument(
+        '-s',
+        action='store_true',
+        help="scans the network and print all the TV's found")
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
 
     args = parser.parse_args()
 
-    loadLog(args.quiet)
+    loadLog(args.q)
     logging.debug('Program started with: %s', sys.argv)
 
-    if args.scan:
-        logging.info("Scanning network...")
+    if args.s:
+        logging.info('Scanning network...')
         tvs = scan_network_ssdp(True, wait=1)
         if not tvs:
             # try again, with a higher timeout
@@ -174,8 +177,8 @@ def main():
         "timeout": 0,
     }
 
-    if args.ip:
-        config['host'] = args.ip
+    if args.i:
+        config['host'] = args.i
     else:
         # no need to scan network if ip is passed as parameter
         tvs = scan_network_ssdp(False)
@@ -188,18 +191,18 @@ def main():
                 logging.error("No Samsung TV found in the network.")
                 sys.exit(0)
 
-    if args.auto:
+    if args.a:
         config['host'] = tvs[0]['ip']
         config['method'] = getMethod(tvs[0]['model'])
         logging.info('Sending command to first TV found: ' + tvs[0]['fn'])
 
-    if args.legacy:
-        config['method'] = "legacy"
+    if args.l:
+        config['method'] = 'legacy'
 
-    if args.key:
-        push(config, args.key)
+    if args.k:
+        push(config, args.k)
 
-    if args.poweroff:
+    if args.p:
         for tv in tvs:
             config['host'] = tv['ip']
             config['method'] = getMethod(tv['model'])
@@ -208,8 +211,8 @@ def main():
             else:
                 logging.error("Turning off " + tv['fn'] + " failed")
 
-    if args.macro:
-        execute_macro(config, args.macro)
+    if args.m:
+        execute_macro(config, args.m)
 
 
 if __name__ == "__main__":
